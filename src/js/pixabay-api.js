5fad76709d5ac5454e25fb9fba760029b2a93a4f
waiolet <violet.bondarenko@gmail.com>
2024-10-05 13:41:09 +0200

    modify

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,11 +5,13 @@ const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchData(inputSearch) {
     const searchParams = new URLSearchParams({
-        key: "46332021-f156325159d295bd8ceb1a335",
+        key: API_KEY,
         q: `${inputSearch}`,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
+        page, 
+        per_page: 15
     });
    try {
     const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
